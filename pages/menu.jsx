793ecaddbbfe667fc947ad565c40fffd44fea4a0
Tomas Lopez <tomas.alp07@gmail.com>
2023-07-13 13:45:04 -0300

    Agregado boton pedidos

--- pages/menu.jsx
+++ pages/menu.jsx
@@ -4,7 +4,7 @@ const Menu = (props) => {
   return(
     <>
       <Header></Header>
-      <div className="w-full h-full bg-white flex flex-col items-center mt-10">
+      <div className="w-full h-full bg-white flex flex-col items-center p-10">
         <h1 className="font-title text-3xl font-light mb-10">Menú de usuario</h1>
         <div className="grid grid-cols-3 gap-10">
           <div className="w-40 h-40 rounded-3xl bg-mediumdark flex flex-col items-center justify-center p-5 hover:bg-dark cursor-pointer transition-all hover:shadow-2xl">
@@ -31,7 +31,10 @@ const Menu = (props) => {
             <img src="/Icons/white/stock.png" alt="" className="h-16 mb-5"/>
             <span className="font-medium text-white text-md font-title">Stock</span>
           </div>
-
+          <div className="w-40 h-40 rounded-3xl bg-mediumdark flex flex-col items-center justify-center p-5 hover:bg-dark cursor-pointer transition-all hover:shadow-2xl">
+            <img src="/Icons/white/pedidos.png" alt="" className="h-16 mb-5"/>
+            <span className="font-medium text-white text-md font-title">Pedidos</span>
+          </div>
         </div>
       </div>
     </>
