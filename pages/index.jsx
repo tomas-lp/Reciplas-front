90afde751d50f0181fe4ac267e2b75e6f51e3a23
Tomas Lopez <tomas.alp07@gmail.com>
2023-07-12 22:55:48 -0300

    Agregado login

---
+++ pages/index.jsx
@@ -0,0 +1,30 @@
+const index = (props) => {
+  return (
+    <div className="min-w-screen min-h-screen bg-mediumdark flex flex-row justify-center items-center">
+      <div className="bg-transparent flex flex-col items-center">
+        <img src="/Logo/logo.png" className="h-52" />
+        <div className="card bg-verylight flex flex-col items-center rounded-2xl py-6 px-10">
+          <h1 className="mt-5 mb-16 font-light text-4xl font-title">
+            Ingresar
+          </h1>
+          <input
+            type="text"
+            placeholder="Usuario"
+            className="text-sm font-title py-2 px-3 w-64 text-left rounded-xl border-2 border-light focus:outline-none focus:border-primary hover:border-mediumlight mb-10 transition-colors"
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            className="text-sm font-title py-2 px-3 w-64 text-left rounded-xl border-2 border-light focus:outline-none focus:border-primary hover:border-mediumlight transition-colors"
+          />
+w
+          <button className="mb-5 mt-10 py-3 px-8 bg-primary text-white font-body font-normal text-md rounded-xl hover:bg-primarydark transition-colors">
+            Iniciar Sesion
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default index
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    fontFamily: {
+      body: ['Inter', '"Open Sans"', 'ui-sans-serif', 'system-ui'],
+      title: ['Poppins', '"Open Sans"', 'ui-sans-serif', 'system-ui'],
+    },
+    colors: {
+      primary: '#41B882',
+      primarydark: '#309467',
+      white: '#ffffff',
+      verylight: '#F1F6F9',
+      light: '#D9D9D9',
+      mediumlight: '#9BA4B5',
+      medium: '#394867',
+      mediumdark: '#212A3E',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
