f3da8c93396e47a1794433ed39f58a55f332b6ea
Tomas Lopez <tomas.alp07@gmail.com>
2023-08-02 17:00:54 -0300

    Adaptado seccion ventas

--- components/ventas/detallesVenta.jsx
+++ components/ventas/detallesVenta.jsx
@@ -3,21 +3,21 @@ import Link from "next/link";
 const DetallesVenta = (props) => {
   return(
     <div className="flex flex-col w-full max-w-screen-xl">
-        <div className="flex flex-row justify-between items-center mb-5 h-12">
-          <div className="flex flex-row items-center">
-            <div className="flex justify-center items-center h-10 px-5 bg-medium text-white rounded-xl mr-5 font-title font-normal text-base"><span>Id: 0000</span></div>
-            <div className="flex justify-center items-center h-10 px-5 bg-verylight rounded-xl font-title font-normal text-base">
-              <span className="mr-8">Cliente: 0000</span>
+        <div className="flex flex-col md:flex-row justify-between items-center mb-5 md:h-12">
+          <div className="flex flex-col w-full md:w-auto md:flex-row items-center mb-5 md:mb-0">
+            <div className="flex w-full md:w-auto mb-2 md:mb-0 md:mr-5 justify-center items-center h-10 px-5 bg-medium text-white rounded-xl font-title font-normal text-base"><span>Id: 0000</span></div>
+            <div className="flex flex-col sm:flex-row w-full md:w-auto justify-center items-center sm:h-10 px-5 bg-verylight rounded-xl font-title font-normal text-base">
+              <span className="sm:mr-8">Cliente: 0000</span>
               <span>Fecha de realización: 01/01/2000</span>
             </div>
           </div>
-          <div className="flex flex-row items-center">
+          <div className="flex flex-row items-center h-12 md:h-auto">
             <div className="bg-primary w-16 p-2 rounded-xl flex justify-center items-center mx-2 h-10 hover:h-12 transition-all cursor-pointer"><img src="/Icons/White/eliminar.png" alt="" className="h-6"/></div>
           </div>
         </div>
 
         <div className="grid grid-cols-1 gap-5">
-          <div className="bg-verylight flex flex-col items-center p-12 rounded-3xl">
+          <div className="bg-verylight flex flex-col items-center px-4 py-12 sm:p-12 rounded-3xl">
             {/* <h2 className="text-black font-title font-light text-2xl mb-5">Productos</h2> */}
             <div className="bg-verylight flex flex-col rounded-3xl w-full">
               <div className="flex flex-row justify-between">
@@ -25,10 +25,10 @@ const DetallesVenta = (props) => {
                   <div className="flex flex-row items-end h-12">
                     <div className="bg-light w-24 p-2 rounded-t-xl flex justify-center mx-2 h-8 hover:h-10 transition-all cursor-pointer"><span className="text-title text-sm text-grey">Producto</span></div>
                     <div className="bg-light w-24 p-2 rounded-t-xl flex justify-center mx-2 h-8 hover:h-10 transition-all cursor-pointer"><span className="text-title text-sm text-grey">Cantidad</span></div>
-                    <div className="bg-light w-24 p-2 rounded-t-xl flex justify-center mx-2 h-8 hover:h-10 transition-all cursor-pointer"><span className="text-title text-sm text-grey">Precio</span></div>
+                    <div className="bg-light w-24 p-2 rounded-t-xl hidden sm:flex justify-center mx-2 h-8 hover:h-10 transition-all cursor-pointer"><span className="text-title text-sm text-grey">Precio</span></div>
                   </div>
                   
-                  <div className="bg-darkgrey w-16 p-2 rounded-t-xl flex justify-center mx-2 h-10 hover:h-12 transition-all cursor-pointer"><img src="/Icons/White/ordenar.png" alt="" className="h-6"/></div>
+                  <div className="bg-darkgrey w-16 py-2 sm:p-2 rounded-t-xl flex justify-center mx-2 h-10 hover:h-12 transition-all cursor-pointer"><img src="/Icons/White/ordenar.png" alt="" className="h-5 sm:h-6"/></div>
                 </div>
               </div>
               <FilaProducto></FilaProducto>
@@ -39,16 +39,16 @@ const DetallesVenta = (props) => {
           </div>
         </div>
 
-        <div className="flex flex-row mt-5">
-          <div className="flex justify-center items-center h-10 px-5 mr-5 bg-primary rounded-xl cursor-pointer hover:bg-primarydark transition-all">
+        <div className="flex flex-col sm:flex-row mt-5">
+          <div className="flex justify-center items-center h-10 px-5 mb-2 sm:mr-5 bg-primary rounded-xl cursor-pointer hover:bg-primarydark transition-all">
             <img src="/Icons/White/facturar.png" alt="" className="h-5 mr-2"/>
             <span className="text-white font-title font-normal text-lg">Facturar</span>
           </div>
-          <div className="flex justify-center items-center h-10 px-5 mr-5 bg-primary rounded-xl cursor-pointer hover:bg-primarydark transition-all">
+          <div className="flex justify-center items-center h-10 px-5 mb-2 sm:mr-5 bg-primary rounded-xl cursor-pointer hover:bg-primarydark transition-all">
             <img src="/Icons/White/imprimir.png" alt="" className="h-5 mr-2"/>
             <span className="text-white font-title font-normal text-lg">Imprimir</span>
           </div>
-          <div className="flex justify-center items-center h-10 px-5 mr-5 bg-primary rounded-xl cursor-pointer hover:bg-primarydark transition-all">
+          <div className="flex justify-center items-center h-10 px-5 mb-2 sm:mr-5 bg-primary rounded-xl cursor-pointer hover:bg-primarydark transition-all">
             <img src="/Icons/White/enviar.png" alt="" className="h-5 mr-2"/>
             <span className="text-white font-title font-normal text-lg">Enviar</span>
           </div>
--- components/ventas/filaProducto.jsx
+++ components/ventas/filaProducto.jsx
@@ -5,7 +5,7 @@ const FilaProducto = (props) => {
     <div className="flex flex-row items-center bg-white h-14 rounded-xl shadow-lg transition-all">
       <div className="flex flex-row justify-center w-24 mx-2"><span className="text-title text-sm text-grey">Ejemplo</span></div>
       <div className="flex flex-row justify-center w-24 mx-2"><span className="text-title text-sm text-grey">100</span></div>
-      <div className="flex flex-row justify-center w-24 mx-2"><span className="text-title text-sm text-grey">0000</span></div>
+      <div className="hidden sm:flex flex-row justify-center w-24 mx-2"><span className="text-title text-sm text-grey">0000</span></div>
     </div>
   )
 }
