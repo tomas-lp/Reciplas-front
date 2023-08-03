e5b000dbd2d60104965923f8243d36a41e9541fa
Tomas Lopez <tomas.alp07@gmail.com>
2023-08-02 23:57:59 -0300

    Corregido detalles stock

--- components/stock/detallesStock.jsx
+++ components/stock/detallesStock.jsx
@@ -8,7 +8,7 @@ const DetallesStock = (props) => {
           <div className="flex justify-center w-full md:w-auto items-center h-10 px-5 bg-verylight rounded-xl mb-2 md:mb-0 md:mr-5 font-title font-normal text-base"><span>Tipo: Productos producidos</span></div>
           <div className="flex justify-center w-full md:w-auto items-center h-10 px-5 bg-verylight rounded-xl font-title font-normal text-base"><span>Fecha de registro: 01/01/2000</span></div>
         </div>
-        <div className="flex flex-row items-center h-12 sm:h-auto">
+        <div className="flex flex-row items-center h-12 lg:h-auto">
           <div className="bg-primary w-16 p-2 rounded-xl flex justify-center items-center mx-2 h-10 hover:h-12 transition-all cursor-pointer"><img src="/Icons/White/imprimir.png" alt="" className="h-6"/></div>
           <Link href="/stock/editar">
             <div className="bg-primary w-16 p-2 rounded-xl flex justify-center items-center mx-2 h-10 hover:h-12 transition-all cursor-pointer"><img src="/Icons/White/editar.png" alt="" className="h-6"/></div>
